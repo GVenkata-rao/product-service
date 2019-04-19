c8111b42bfb304e1c09ca8f97c1bf531a6e37a36
Venkatarao Gude <venkatarao_gude@epam.com>
2019-04-19 16:32:20 +0530

    added user model class

---
+++ src/main/java/com/prodservice/model/User.java
@@ -0,0 +1,44 @@
+package com.prodservice.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+    private Integer userId;
+    private String userName;
+    private String email;
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
